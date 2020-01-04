1374af52d4def5ac07efc42cf1e670a44d4ecc16
Jiří Šafra <jiri.safra@seznam.cz>
2020-01-04 17:44:14 +0100

    user_input rewrited to return any input which is valid to desired rules

--- blackjack_cli.py
+++ blackjack_cli.py
@@ -1,43 +1,52 @@
 '''Helper module for a blackjack game'''
 
-def user_input(**kwargs):
+import re
+
+def user_input(prompt = "", pattern = None, expected_type = str):
     '''Calls for user input from CLI with defined prompt.
 
-    It is possible to adjust to actual need by set of optional keywords (look at Other parameters section.)
-
-    `print(user_input(prompt="Neco mi rekni:", accept_values=['yes', 'no'], case_sensitive=True))`
-
-    Other parameters
+    Parameters
     ----------------
     prompt : `str`, optional
         Message to be shown to user.
-    accept_values : `list`, optional
-        List of values to be accepted. If not specified any input is accepted. Default `[]`
-    case_sensitive : `bool`, optional
-        If `True` then uppercase and lowercase letters are treated as distinct. Default `False`
+    pattern : `str` or `re.Pattern`, optional
+        Regex pattern used for user input validation.
+        Works only if expected_type param is not set or if `str` is its value.
+    expected_type : `class`, optional
+        Expected type of return value. User input is accepted only if it is possible to cast it to expected type.
+        Default `str`.
 
     Returns
     -------
-    str
-        Text entered by user
+    `expected_type`
+        Value entered by user
+
+    Examples
+    --------
+        x1 = user_input(prompt="Tell me something: ")
+
+        x2 = user_input(prompt="Tell me something: ", pattern="^a.*$")
+
+        x3 = user_input(prompt="Tell me something: ", expected_type=float)  
     '''
+    regex_validation = True if expected_type == str and pattern else False
+    type_validation = expected_type != str
 
-    accept_values = kwargs.get("accept_values", [])
-    prompt = kwargs.get("prompt", "Give me some input please:")
-    case_sensitive = kwargs.get("case_sensitive", False)
+    if regex_validation and type(pattern) == str:
+        pattern = re.compile(pattern)
 
-    if accept_values:     # Empty list is evaluated as False
-        prompt.format("/".join(accept_values))
-    if not case_sensitive:
-        accept_values = [av.lower() for av in accept_values]
-
-    raw_input = input(prompt) if case_sensitive else input(prompt).lower()
-    
-    while accept_values and raw_input not in accept_values:   # Check user input against accepted values
-        print("I'm sorry I don't understand. Please answer {}:".format("/".join(accept_values)))
-        raw_input = input(prompt) if case_sensitive else input(prompt).lower()
-    
-    return raw_input
+    while regex_validation or type_validation:
+        raw_input = input(prompt)
+        if type_validation:
+            try:
+                raw_input = expected_type(raw_input)
+                return raw_input
+            except Exception:
+                pass
+        elif regex_validation:
+            if pattern.match(raw_input):
+                return raw_input
+        print("Invalid input. Please answer in correct format.")
 
 def label_print(message, decoration = "-", extra_line = True):
     '''Prints a message in an ascii frame.
@@ -60,6 +69,5 @@ def label_print(message, decoration = "-", extra_line = True):
         print()
 
 if __name__ == "__main__":
-    a = user_input(prompt="Neco mi rekni:", accept_values=['yes', 'no'], case_sensitive=True)
-
-    label_print("You said " + a)
+    a = user_input(prompt="Tell me something: ", pattern="^a.*$", expected_type=float)
+    label_print("You said {} and it is {}".format(a, type(a))) 
